a84607dc2b9dafe8111840b4fbf61e9d4a5a781e
AlyssaEdwards13 <edwardsalyssa83@gmail.com>
2018-06-08 18:56:44 +0000

    automatically backed up by learn

---
+++ nested_data.rb
@@ -0,0 +1,11 @@
+# CREATE A HASG WITH NESTED ARRAYS OF YOUR GROUPS FAVORITE has_and_belongs_to_many
+
+# CREATE A HASH 
+
+hello {
+  :Emi => ["Come on Elieen", "Pitiful Children", "Dancing Quuen"]
+  :Amira => ["Mine", "Call out my name", "Bartier"]
+  :alyssa => ["fav song 1", "fav song 2", "fav song 3"]
+}
+
+puts hello [amira]
